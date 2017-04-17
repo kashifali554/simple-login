09e02d82040100a44e193255dddc4c3c1896c4ab
Kashif Ali <kashifali874@gmail.com>
2017-04-17 01:26:34 -0700

    first commit

---
+++ public/scripts/app.js
@@ -0,0 +1,25 @@
+// console.log("Sanity Check")
+$(document).ready(function (argument) {
+  // // console.log("app.js is conacted")
+  // // select the form and serialize its data
+  // var signupData = $("#signup-form").serialize();
+  // // console.log(signupData);
+  // // send POST request to /users with the form data
+  // $.post('/users', signupData, function(response){
+  //   // console.log(response);
+  // })
+
+  //   $('#login-form').on('submit', function(e) {
+  //   e.preventDefault();
+  //   // select the form and serialize its data
+  //   // note: this is the form because the event handler
+  //   //   was triggered from the form
+  //   var loginData = $(this).serialize();
+  //   console.log(loginData)
+  //   // send POST request to /login with the form data
+  //   $.post('/sessions', loginData, function(response) {
+  //     console.log(response);
+  //   });
+  // });
+
+})
